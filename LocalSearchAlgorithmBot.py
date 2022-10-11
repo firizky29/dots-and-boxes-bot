249bc5f42d6d6c9999ebd71d41e40970571de444
Muhammad Fahmi Irfan <66018985+DeeGeeDow@users.noreply.github.com>
2022-10-11 15:00:34 +0700

    mengcommit local search

--- LocalSearchAlgorithmBot.py
+++ LocalSearchAlgorithmBot.py
@@ -2,9 +2,28 @@ from GameAction import GameAction
 from GameState import GameState
 from Bot import Bot
 
-class LocalSearchAlgorithmBot(Bot):
-    
-    def __init__():        
-        
+class state:
 
-    def 
+    def __init__(self, ):
+        self.value = 0
+    
+class LocalSearchBot(Bot):
+    """
+    An interface for bot. Inherit it to create your own bots!
+    """
+    def get_action(self, state: GameState) -> GameAction:
+        """
+        Returns action based on state.
+        """
+        raise NotImplementedError()
+    
+    def probability(self, deltaE, temperature):
+        return math.exp(deltaE, temperature)
+    
+    def schedule(self, t):
+        temp = 100 - 2*t
+        if temp>0:
+            return temp
+        else:
+            return 0
+    
