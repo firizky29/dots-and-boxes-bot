2071ddcbc7aa1e53bcc9a364a3adf5cb8ea9d590
firizky29 <13520095@std.stei.itb.ac.id>
2022-10-16 18:36:07 +0700

    local search

--- LocalSearchAlgorithmBot.py
+++ LocalSearchAlgorithmBot.py
@@ -1,27 +1,42 @@
 from GameAction import GameAction
 from GameState import GameState
 from Bot import Bot
+import math
 
-class state:
 
-    def __init__(self, ):
-        self.value = 0
     
 class LocalSearchBot(Bot):
-    """
-    An interface for bot. Inherit it to create your own bots!
-    """
+    
+    def __init__(self, isPlayer1: bool, temperature: int):
+        self.isPlayer1 = isPlayer1
+        self.T = temperature
+        
+
+
     def get_action(self, state: GameState) -> GameAction:
         """
         Returns action based on state.
         """
         raise NotImplementedError()
+
+    def get_random_state(self, state: GameState) -> GameState:
+        """
+        Returns a random state
+        """
+        raise NotImplementedError()
+    
+    def get_state_value(self, state: GameState) -> int:
+        """
+        Returns a value of state
+        """
+        raise NotImplementedError()
+
     
     def probability(self, deltaE, temperature):
         return math.exp(deltaE, temperature)
     
     def schedule(self, t):
-        temp = 100 - 2*t
+        temp = self.T - 2*t
         if temp>0:
             return temp
         else:
